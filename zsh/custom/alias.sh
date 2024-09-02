fb726a1e34f5e3a70da7c3525f5f7e981ea63c31
vhqkze <zqz0601@gmail.com>
2024-09-02 19:05:03 +0800

    zsh: 调整环境变量

--- zsh/custom/alias.sh
+++ zsh/custom/alias.sh
@@ -1,7 +1,6 @@
-alias git="LANG=zh_CN.UTF-8 git"
 alias eza="eza --time-style=+%Y-%m-%d\ %H:%M:%S --no-quotes -l"
 alias exa="eza --time-style=+%Y-%m-%d\ %H:%M:%S --no-quotes -l"
 
 alias rm="rm -r"
 alias cp="cp -r"
-
+alias n=nvim
