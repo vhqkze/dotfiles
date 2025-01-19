3cb253dda6389e79c7a4e363ffab8993fa5ef992
vhqkze <zqz0601@gmail.com>
2025-01-19 18:14:13 +0800

    zsh: 增加代理配置命令proxy

--- zsh/custom/func.sh
+++ zsh/custom/func.sh
@@ -23,3 +23,30 @@ tte() {
 }
 
 app() { (cd "$HOME"/Developer/minitools && poetry run python track/app.py "$@"); }
+
+proxy() {
+    if [ $# -eq 0 ]; then
+        echo "http_proxy=$http_proxy"
+        echo "https_proxy=$https_proxy"
+        echo "ftp_proxy=$ftp_proxy"
+        echo "socks_proxy=$socks_proxy"
+        echo "all_proxy=$all_proxy"
+    elif [[ "$1" == "off" ]]; then
+        unset http_proxy https_proxy ftp_proxy socks_proxy all_proxy
+        echo "proxy off"
+    elif [[ "$1" =~ ^[0-9]+$ ]]; then
+        export http_proxy="127.0.0.1:$1"
+        export https_proxy="127.0.0.1:$1"
+        export ftp_proxy="127.0.0.1:$1"
+        export socks_proxy="127.0.0.1:$1"
+        export all_proxy="127.0.0.1:$1"
+        echo "set proxy to 127.0.0.1:$1"
+    else
+        export http_proxy="$1"
+        export https_proxy="$1"
+        export ftp_proxy="$1"
+        export socks_proxy="$1"
+        export all_proxy="$1"
+        echo "set proxy to $1"
+    fi
+}
