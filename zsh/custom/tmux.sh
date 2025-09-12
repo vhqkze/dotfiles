6ff142dc373b2174b200822bedfd87f6cdf63753
vhqkze <zqz0601@gmail.com>
2025-09-12 15:25:56 +0800

    tmux: 整理配置，拆分样式为独立文件，增加seamless.nvim插件
    - 调整了配置结构
    - 将样式设置独立为`theme.conf`文件
    - 移除导航快捷键，改为使用`seamless.nvim`插件
    - 移除`tmux-notify`插件
    - zsh的`preexec_functions`增加更新函数，更新env，配合tmux配置文件中`set-option -g
    update-environment`，让这几个环境变量的值能够正确更新

--- zsh/custom/tmux.sh
+++ zsh/custom/tmux.sh
@@ -4,3 +4,10 @@ if [[ -n "$SSH_CONNECTION" ]]; then
     #if not inside a tmux session, and if no session is started, start a new session
     test -z "$TMUX" && (tmux attach || tmux new-session)
 fi
+
+if [[ -n "$TMUX" ]]; then
+    tmux_update_env() {
+        eval "$(tmux showenv -s)"
+    }
+    preexec_functions+=(tmux_update_env)
+fi
