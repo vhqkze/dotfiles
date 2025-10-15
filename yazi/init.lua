dbeedf96e6365986609de0684179fa4aac380433
vhqkze <zqz0601@gmail.com>
2025-10-15 18:43:39 +0800

    yazi: 更新配置

--- yazi/init.lua
+++ yazi/init.lua
@@ -2,33 +2,28 @@
 
 --- Show symlink in status bar
 ---@see https://yazi-rs.github.io/docs/tips#symlink-in-status
----@source yazi-plugin/preset/components/status.lua
-function Status:name()
-    local h = self._tab.current.hovered
-    if not h then
-        return ui.Line({})
+Status:children_add(function(self)
+    local h = self._current.hovered
+    if h and h.link_to then
+        return " -> " .. tostring(h.link_to)
+    else
+        return ""
     end
-
-    local linked = ""
-    if h.link_to ~= nil then
-        linked = " -> " .. tostring(h.link_to)
-    end
-    return ui.Line(" " .. h.name .. linked)
-end
+end, 3300, Status.LEFT)
 
 --- Show user/group of files in status bar
 ---@see https://yazi-rs.github.io/docs/tips#user-group-in-status
 Status:children_add(function()
     local h = cx.active.current.hovered
-    if h == nil or ya.target_family() ~= "unix" then
-        return ui.Line({})
+    if not h or ya.target_family() ~= "unix" then
+        return ""
     end
 
     return ui.Line({
         ui.Span(ya.user_name(h.cha.uid) or tostring(h.cha.uid)):fg("magenta"),
-        ui.Span(":"),
+        ":",
         ui.Span(ya.group_name(h.cha.gid) or tostring(h.cha.gid)):fg("magenta"),
-        ui.Span(" "),
+        " ",
     })
 end, 500, Status.RIGHT)
 
